f456be66613e4ee47906fdfef6d0e93a185eecfd
Agustin Berge <agustinberge@gmail.com>
2026-09-06 11:58:31 +0200

    Add empty summary test case

---
+++ test/summary.no_cases.cpp
@@ -0,0 +1,10 @@
+// Eggs.Test
+//
+// Copyright Agustin K-ballo Berge, Fusion Fenix 2026
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#include <eggs/test.hpp> // IWYU pragma: keep
+
+// Intentionally empty: a registry with no TEST_CASEs at all.
